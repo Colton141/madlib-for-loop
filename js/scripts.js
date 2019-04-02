5c8b5bb87d1722449d0a0d19a67cdefa00f175c1
Brian Hensley and Colton Lacey <brian@>
2019-04-02 10:25:04 -0700

    change .foreach method to for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,26 @@
+// $(document).ready(function() {
+//   $('form').submit(function(event) {
+//     var userAnswers = ["verb1", "place", "noun", "verb2", "adjective"]
+//     userAnswers.forEach(function(userAnswer) {
+//       var storyOutput = $("input#" + userAnswer).val();
+//       // var verb1Input = $('input#verb1').val();
+//       $("." + userAnswer).text(storyOutput);
+//       // $('.verb1').text(verb1Input);
+//     })
+//
+//     $('#story').show();
+//     event.preventDefault();
+//   });
+// });
+
+
 $(document).ready(function() {
   $('form').submit(function(event) {
-    var userAnswers = ["verb1", "place", "noun", "verb2", "adjective"]
-    userAnswers.forEach(function(userAnswer) {
-      var storyOutput = $("input#" + userAnswer).val();
-      // var verb1Input = $('input#verb1').val();
-      $("." + userAnswer).text(storyOutput);
-      // $('.verb1').text(verb1Input);
-    })
+    var userAnswers = ["verb1", "place", "noun", "verb2", "adjective"];
+    for (index = 0; index < userAnswers.length; index +=1) {
+      alert($("input#" + userAnswers[index]).val());
+      $("." + userAnswers[index]).text($("input#" + userAnswers[index]).val())
+    }
 
     $('#story').show();
     event.preventDefault();
